fead997ea794ae1c2a46bb4fbaf2fa3f4b779a54
Matthew Benchimol <mdbench@outlook.com>
2025-01-30 01:03:38 -0500

    minor re-correction to health-check script
    Signed-off-by: Matthew Benchimol <mdbench@outlook.com>

--- health-check.sh
+++ health-check.sh
@@ -1,7 +1,7 @@
 # Curated News has heavily modified the original Statsig shell script with a more robust reachability check, including check responses by headers and verifying them by date.
 commit=false
 origin=$(git remote get-url origin)
-if [[ $origin == *CuratedNews/business* ]]
+if [[ $origin == *CuratedNews/statuses* ]]
 then
   commit=true
 fi
